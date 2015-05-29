fce085ba0b9ad0f1ea4469f4da44f6dfe721a57f
Piotr Szul <piotr.szul@csiro.au>
2015-05-29 15:06:35 +1000

    Fixex type

--- src/main/scala/name/szul/edu/kaggle/facebook/HiveSQLFeatureProviderWithParam.scala
+++ src/main/scala/name/szul/edu/kaggle/facebook/HiveSQLFeatureProviderWithParam.scala
@@ -5,16 +5,7 @@ import scala.reflect.ClassTag
 import org.apache.spark.sql.catalyst.expressions.Row
 
 class HiveSQLFeatureProviderWithParam[T](baseName:String, query:String, param:String, version:Int=1)(implicit hc:HiveContext, implicit val stag:ClassTag[T]) extends FeatureProvider[T] {
-  
-  
-  def get(r:Row, i:Int):T = {
-    typeName match {
-      case "long" => r.getLong(i).asInstanceOf[T]
-      case "double" => r.getDouble(i).asInstanceOf[T]
-      case _ => throw new RuntimeException("unknown type: " + typeName)
-    }
-  }
-  
+    
   @Override
   val name = baseName + "_" + param
   
@@ -22,7 +13,7 @@ class HiveSQLFeatureProviderWithParam[T](baseName:String, query:String, param:St
   def compute:Iterable[Tuple2[String,T]] = {
     //new 
     hc.sql(String.format(query,param)
-        ).map({r => println(r); (r(0).toString(), get(r,1))}).collect().toList
+        ).map({r => println(r); (r(0).toString(),r.getAs(1)) }).collect().toList
   }
 
 }
