a9ce811ef602856a03f5d0bd2100b9983022a5e4
Piotr Szul <piotr.szul@csiro.au>
2015-05-30 09:28:14 +1000

    MODEL: rf-150530_092118-0.89838

--- R/train_rf.R
+++ R/train_rf.R
@@ -1,6 +1,17 @@
 library(caret)
 library(pROC)
 
+printf <- function(...) invisible(print(sprintf(...)))
+commit <- function() {
+    modelId <-paste(model$method, format(model$timestamp, "%y%m%d_%H%M%S"), sprintf('%0.5f', estAUC), sep='-' )
+    printf("Committing model: %s", modelId)
+    write.csv(resultTest,sprintf('models/%s.csv', modelId), quote=FALSE,row.names=FALSE)
+    write.csv(resultTrain,sprintf('models/meta/train_%s.csv', modelId), quote=FALSE,row.names=FALSE)    
+    save(model,file=sprintf('models/meta/%s.Rdata', modelId)) 
+    system("git add .")
+    system(sprintf('git commit -m "MODEL: %s"', modelId))
+}
+
 all_data <- read.csv('target/data.csv')
 
 test_data <- all_data[is.na(all_data$outcome),]
@@ -12,28 +23,38 @@ train_set <- subset(train_data, select=-c(bidder_id,outcome, per_auction_avg_auc
 set.seed(37)
 
 multiLogLoss <-function (data, lev = NULL, model = NULL) {
-    ro <-roc(data$obs, data$c_1, levels(c('c_0',c_1)))
+    ro <-roc(data$obs, data$c_1, levels(c('c_0','c_1')))
     c(ROC=ro$auc)
 }
 
 trControl <- trainControl(method="cv",
                           number=10,
-                          verboseIter=FALSE,
+                          verboseIter=TRUE,
                           classProbs = TRUE, 
                           summaryFunction = multiLogLoss,
                           allowParallel = FALSE)
 
-rfModel <- train(class ~ ., data = train_set, 
+model <- train(class ~ ., data = train_set, 
                  method="rf",
                  ntree=500,
+                 importance = FALSE,
                  trControl = trControl,
                  metric = "ROC",
-                 maximize = TRUE
+                 maximize = TRUE, 
+                 tuneLength = 5
 )
 
+model$timestamp <-Sys.time()
 print("Best:")
-rfModel$results[as.numeric(row.names(rfModel$bestTune)),]
+model$results[as.numeric(row.names(model$bestTune)),]
+estAUC<-model$results[as.numeric(row.names(model$bestTune)),2]
+printf("Estimated AUD %f" , estAUC)
+predTrain <- predict(model, newdata=train_data,type="prob")
+resultTrain <- data.frame(bidder_id=train_data$bidder_id,prediction=predTrain$c_1)
+trainROC <-roc(train_data$class, predTrain$c_1, levels(c('c_0','c_1')))
+trainAUC <-trainROC$auc 
+printf("Train AUC: %f", trainAUC)
+predTest <- predict(model, newdata = test_data,type="prob")
+resultTest <- data.frame(bidder_id=test_data$bidder_id,prediction=predTest$c_1)
 
-pred <- predict(rfModel, newdata = test_data,type="prob")
-result <- data.frame(bidder_id=test_data$bidder_id,prediction=pred$c_1)
-write.csv(result,'target/result.csv', quote=FALSE,row.names=FALSE)
+
