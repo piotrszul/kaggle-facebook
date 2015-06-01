8e8b24dffab07e85015d393f2374219bedc708d1
Piotr Szul <piotr.szul@csiro.au>
2015-06-01 13:50:21 +1000

    MODEL: gbm-150601_134843-0.92487

--- R/train_gbm.R
+++ R/train_gbm.R
@@ -18,7 +18,7 @@ test_data <- all_data[is.na(all_data$outcome),]
 train_data <- all_data[!is.na(all_data$outcome),]
 
 train_data[,"class" ] = as.factor(paste("c", train_data$outcome, sep='_'))
-train_set <- subset(train_data, select=-c(bidder_id,outcome, per_auction_avg_auction))
+train_set <- subset(train_data, select=-c(bidder_id,outcome, per_auction_avg_auction, bids_per_bin_500_dev, bids_per_bin_500_avg, bids_per_bin_500_min, bids_per_bin_500_median, bids_per_bin_500_max ))
 
 set.seed(37)
 
@@ -27,7 +27,7 @@ multiLogLoss <-function (data, lev = NULL, model = NULL) {
     c(ROC=ro$auc)
 }
 
-tuningGrid <- expand.grid(n.trees = c(500, 1000), interaction.depth = c(3,6,9), shrinkage = c(0.05, 0.01, 0.005))
+tuningGrid <- expand.grid(n.trees = c(500, 1000, 1500), interaction.depth = c(3,9,18), shrinkage = c(0.05, 0.01, 0.005))
 
 folds <- createFolds(train_set$class, 10, returnTrain=TRUE)
 trControl <- trainControl(method="cv",
